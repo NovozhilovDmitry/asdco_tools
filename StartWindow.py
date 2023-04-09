5cbf7de4c71142d05ece3c218a0bec94c5a7f1de
Dmitry <novozhilovdmitry93@gmail.com>
2023-04-09 13:18:20 +0500

    add pictures, schemas buttons and fields for dumps path

--- StartWindow.py
+++ StartWindow.py
@@ -1,6 +1,8 @@
+import pathlib
 import sys
 import traceback
 from myLogging import logger
+from PyQt6.QtGui import QPixmap, QIcon
 from PyQt6.QtCore import QRunnable, QThreadPool, QSettings, QObject, pyqtSignal, pyqtSlot
 from PyQt6.QtWidgets import (QMainWindow,
                              QWidget,
@@ -18,7 +20,8 @@ from PyQt6.QtWidgets import (QMainWindow,
                              QTableWidgetItem,
                              QHeaderView,
                              QPlainTextEdit,
-                             QProgressBar)
+                             QProgressBar,
+                             QFileDialog)
 from functions import (get_string_show_pdbs,
                        delete_temp_directory,
                        runnings_sqlplus_scripts_with_subprocess,
@@ -70,6 +73,7 @@ class Window(QMainWindow):
         super(Window, self).__init__()
         self.setWindowTitle("ASDCO TOOLS")  # заголовок главного окна
         self.setMinimumSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.btn_icon = QPixmap("others/hedgehog.png")
         self.layout = QWidget()
         self.main_layout = QVBoxLayout()
         self.top_grid_layout = QGridLayout()
@@ -301,6 +305,25 @@ class Window(QMainWindow):
         else:
             self.schemas[checkbox.text()] = 0
 
+    def fn_set_path_for_dumps(self):
+        button = self.sender()
+        project_path = pathlib.Path.cwd()
+        get_dir = QFileDialog.getExistingDirectory(self, caption='Выбрать файл')
+        if get_dir:
+            get_dir = get_dir
+        else:
+            get_dir = 'Путь не выбран'
+        if button is self.btn_path_schema1:
+            self.path_schema1.setText(get_dir)
+        elif button is self.btn_path_schema2:
+            self.path_schema2.setText(get_dir)
+        elif button is self.btn_path_schema3:
+            self.path_schema3.setText(get_dir)
+        elif button is self.btn_path_schema4:
+            self.path_schema4.setText(get_dir)
+        elif button is self.btn_path_schema5:
+            self.path_schema5.setText(get_dir)
+
     def closeEvent(self, event):
         """
         :param event: событие, которое можно принять или переопределить при закрытии
@@ -468,22 +491,57 @@ class Window(QMainWindow):
         self.btn_create_schema.clicked.connect(self.creating_schemas)
         self.btn_delete_schema = QPushButton('Удалить схему')
         self.btn_delete_schema.clicked.connect(self.deleting_schemas)
+        self.path_schema1 = QLineEdit()
+        self.btn_path_schema1 = QPushButton()
+        self.btn_path_schema1.setIcon(QIcon(self.btn_icon))
+        self.path_schema1.setPlaceholderText('Введите путь или нажмите на кнопку')
+        self.btn_path_schema1.clicked.connect(self.fn_set_path_for_dumps)
+        self.path_schema2 = QLineEdit()
+        self.btn_path_schema2 = QPushButton()
+        self.btn_path_schema2.setIcon(QIcon(self.btn_icon))
+        self.path_schema2.setPlaceholderText('ВВведите путь или нажмите на кнопку')
+        self.btn_path_schema2.clicked.connect(self.fn_set_path_for_dumps)
+        self.path_schema3 = QLineEdit()
+        self.btn_path_schema3 = QPushButton()
+        self.btn_path_schema3.setIcon(QIcon(self.btn_icon))
+        self.path_schema3.setPlaceholderText('Введите путь или нажмите на кнопку')
+        self.btn_path_schema3.clicked.connect(self.fn_set_path_for_dumps)
+        self.path_schema4 = QLineEdit()
+        self.btn_path_schema4 = QPushButton()
+        self.btn_path_schema4.setIcon(QIcon(self.btn_icon))
+        self.path_schema4.setPlaceholderText('Введите путь или нажмите на кнопку')
+        self.btn_path_schema4.clicked.connect(self.fn_set_path_for_dumps)
+        self.path_schema5 = QLineEdit()
+        self.btn_path_schema5 = QPushButton()
+        self.btn_path_schema5.setIcon(QIcon(self.btn_icon))
+        self.path_schema5.setPlaceholderText('Введите путь или нажмите на кнопку')
+        self.btn_path_schema5.clicked.connect(self.fn_set_path_for_dumps)
         self.input_schemas_area = QTextEdit()
         self.tab_schemas.layout.addWidget(self.checkbox_schema1, 0, 0)
         self.tab_schemas.layout.addWidget(self.input_schema1_name, 0, 1)
         self.tab_schemas.layout.addWidget(self.input_schema1_pass, 0, 2)
+        self.tab_schemas.layout.addWidget(self.path_schema1, 0, 3)
+        self.tab_schemas.layout.addWidget(self.btn_path_schema1, 0, 4)
         self.tab_schemas.layout.addWidget(self.checkbox_schema2, 1, 0)
         self.tab_schemas.layout.addWidget(self.input_schema2_name, 1, 1)
         self.tab_schemas.layout.addWidget(self.input_schema2_pass, 1, 2)
+        self.tab_schemas.layout.addWidget(self.path_schema2, 1, 3)
+        self.tab_schemas.layout.addWidget(self.btn_path_schema2, 1, 4)
         self.tab_schemas.layout.addWidget(self.checkbox_schema3, 2, 0)
         self.tab_schemas.layout.addWidget(self.input_schema3_name, 2, 1)
         self.tab_schemas.layout.addWidget(self.input_schema3_pass, 2, 2)
+        self.tab_schemas.layout.addWidget(self.path_schema3, 2, 3)
+        self.tab_schemas.layout.addWidget(self.btn_path_schema3, 2, 4)
         self.tab_schemas.layout.addWidget(self.checkbox_schema4, 3, 0)
         self.tab_schemas.layout.addWidget(self.input_schema4_name, 3, 1)
         self.tab_schemas.layout.addWidget(self.input_schema4_pass, 3, 2)
+        self.tab_schemas.layout.addWidget(self.path_schema4, 3, 3)
+        self.tab_schemas.layout.addWidget(self.btn_path_schema4, 3, 4)
         self.tab_schemas.layout.addWidget(self.checkbox_schema5, 4, 0)
         self.tab_schemas.layout.addWidget(self.input_schema5_name, 4, 1)
         self.tab_schemas.layout.addWidget(self.input_schema5_pass, 4, 2)
+        self.tab_schemas.layout.addWidget(self.path_schema5, 4, 3)
+        self.tab_schemas.layout.addWidget(self.btn_path_schema5, 4, 4)
         self.tab_schemas.layout.addWidget(self.btn_create_schema, 5, 1)
         self.tab_schemas.layout.addWidget(self.btn_delete_schema, 5, 2)
         self.tab_schemas.layout.addWidget(self.input_schemas_area, 6, 0, 1, 4)
@@ -497,8 +555,7 @@ if __name__ == '__main__':
     sys.exit(app.exec())
 
 # добавить progressbar в выполнение функции
-# сделать дополнительное поле в схемах (там будет путь к дампам)
-# рядом с полем (или в поле) добавить иконку для вызова диалогового окна к дампам
-# сделать функцию (или 2) для определения формата дампа -> если архив, то разархивировать, иначе просто берем дамп
-# падает программа если не проверены бд и не заполнен список
+# сделать функцию (или 2) для определения формата дампа ->
+# если архив, то предложение разархивировать, если дамп, то берем дамп
+# падает программа если не проверены бд и не заполнен список?
 # после клонирования (удаления?) почему-то выводится ошибка. посмотреть номер ошибки и текст
